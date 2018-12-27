18ee7633531c9dea2a96b7028aab2bb631a0d4c0
Alatra <gudash.35@gmail.com>
2018-12-27 16:20:01 +0100

    speed test

--- SpeedTest.java
+++ SpeedTest.java
@@ -1,7 +1,34 @@
+import java.util.*;
+
 public class SpeedTest {
 
-    public SpeedTest(int Nmax, int k, InputFactory iFactory, OutputFactory oFactory) {
-        
-        
+
+    public void speedTestIntPut(int N, int k, InputFactory iFactory) {
+        List<Input> streamToTest = new LinkedList<Input>();
+        for(int i = 0 ; i < k ; i++ ) {
+            streamToTest.add(iFactory.getFreshInputStream("a"));
+        }
+        ListIterator<Input> it = streamToTest.listIterator();
+        for(int j = 0 ; j < k ; j++ ) {
+            Input test = it.next();
+            for(int i = 0 ; i < N ; i++ ) {
+                test.readNext();
+            }
+        }
     }
+
+    public void speedTestOutput(int N, int k, OutputFactory oFactory,Generator gen) {
+        List<Output> streamToTest = new LinkedList<Output>();
+        for(int i = 0 ; i < k ; i++ ) {
+            streamToTest.add(oFactory.getFreshOutputStream("a"));
+        }
+        ListIterator<Output> it = streamToTest.listIterator();
+        for(int j = 0 ; j < k ; j++ ) {
+            Output test = it.next();
+            for(int i = 0 ; i < N ; i++ ) {
+                test.write(gen.generate());
+            }
+        }
+    }
+
 }
